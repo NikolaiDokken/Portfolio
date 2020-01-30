6c18afb6e6de08120493cd7fd06da1938571a691
Nikolard <nikolard@stud.ntnu.no>
2020-01-31 00:02:02 +0100

    frontpage v1 done

--- client/src/views/Frontpage/Frontpage.jsx
+++ client/src/views/Frontpage/Frontpage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Grid from "@material-ui/core/Grid";
-import { Typography, makeStyles } from "@material-ui/core";
+import {
+  Typography,
+  makeStyles,
+  ThemeProvider,
+  responsiveFontSizes,
+  createMuiTheme
+} from "@material-ui/core";
+import "./Frontpage.css";
 
 const useStyles = makeStyles({
   body: {
@@ -13,20 +20,42 @@ export default function Frontpage() {
   const classes = useStyles();
 
   return (
-    <Grid container direction="row" className={classes.body}>
-      <Grid container item xs justify="center">
-        <Avatar
-          alt="Nikolai Dokken"
-          src="./resources/test.png"
-          style={{ height: "500px", width: "500px" }}
-        />
+    <div>
+      <Grid container direction="row" className={classes.body}>
+        <Grid container item xs={12} sm={6} justify="center">
+          <Avatar
+            alt="Nikolai Dokken"
+            src="./resources/test.png"
+            style={{ height: "40vw", width: "40vw" }}
+          />
+        </Grid>
+        <Grid container item xs={12} sm={6} justify="center" direction="column">
+          <ThemeProvider theme={responsiveFontSizes(createMuiTheme())}>
+            <Typography variant="h1">Nikolai Roede Dokken</Typography>
+            <Typography variant="h5">
+              - Computer Engineering at NTNU Trondheim
+            </Typography>
+          </ThemeProvider>
+        </Grid>
       </Grid>
-      <Grid container item xs justify="center" direction="column">
-        <Typography variant="h1">Nikolai Roede Dokken</Typography>
-        <Typography variant="h5">
-          - Computer Engineering at NTNU Trondheim
-        </Typography>
+      <Grid container direction="row">
+        <Grid container item xs={12} justify="center">
+          <div id="flipBox">
+            <Typography variant="h6">MADE WITH</Typography>
+            <div id="flip">
+              <div>
+                <div>react</div>
+              </div>
+              <div>
+                <div>MATERIAL UI</div>
+              </div>
+              <div>
+                <div>CSS</div>
+              </div>
+            </div>
+          </div>
+        </Grid>
       </Grid>
-    </Grid>
+    </div>
   );
 }
