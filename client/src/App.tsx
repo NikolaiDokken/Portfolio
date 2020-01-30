72a1b1afb121e5b6400edeef1d59992fc4c7344f
Nikolard <nikolard@stud.ntnu.no>
2020-01-30 12:01:57 +0100

    clean folder structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,7 @@ import React from "react";
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <header className="App-header">Hello</header>
     </div>
   );
 };
