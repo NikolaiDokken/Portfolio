410640574134cee49159a6c1b4447abb42b17144
NikolaiDokken <nikolaidokken@live.no>
2020-02-03 07:28:54 +0100

    fixed roller position frontpage

--- client/src/views/Overview/Overview.jsx
+++ client/src/views/Overview/Overview.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { makeStyles, Button } from "@material-ui/core";
-import "./Overview.css";
 
 const useStyles = makeStyles({
   button: {
     borderColor: "#fff",
     color: "#fff",
     margin: "10px"
+  },
+  bouncy: {
+    animation: "bouncy 5s infinite linear",
+    position: " relative"
+  },
+  "@keyframes bouncy": {
+    "0%": { top: "0em" },
+    "40%": { top: "0em" },
+    "43%": { top: "-0.9em" },
+    "46%": { top: "0em" },
+    "48%": { top: "-0.4em" },
+    "50%": { top: "0em" },
+    "100%": { top: "0em" }
   }
 });
 
@@ -14,7 +26,7 @@ export default function(props) {
   const classes = useStyles();
 
   return (
-    <div>
+    <div className={classes.bouncy}>
       <Button
         variant="outlined"
         className={classes.button}
