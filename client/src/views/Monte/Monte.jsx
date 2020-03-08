11fb3ba8cad1ec85d259b6f8e41c9ca6aa2f2238
Nikolard <nikolard@stud.ntnu.no>
2020-03-08 23:38:53 +0100

    added lego monte

--- client/src/views/Monte/Monte.jsx
+++ client/src/views/Monte/Monte.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Grid, Typography, makeStyles } from "@material-ui/core";
+import SkillChips from "../../components/Chips/SkillChips";
 
 const useStyles = makeStyles({
   body: { color: "white" }
@@ -10,8 +11,8 @@ export default function Monte(props) {
   return (
     <Grid
       container
-      direction="column"
       justify="center"
+      alignItems="center"
       className={classes.body}
       style={
         props.mobile
@@ -19,19 +20,26 @@ export default function Monte(props) {
           : { margin: "0 20px 0 50px", width: "100%", height: "100%" }
       }
     >
-      <Grid item>
-        <Typography variant="h2">Monte Lego Robot</Typography>
+      <Grid container direction="column" item xs={12} md={6}>
+        <Typography variant="h3">Monte Lego Robot</Typography>
+        <Typography variant="h6">Team project</Typography>
+        <Typography variant="body">
+          Given a Created a LEGO robot of sorts. We decided to make a street
+          hustler. The player starts by selecting one out of three difficulties.
+          The robot mixes the "cups" and the player finally guesses one. Both
+          choices are done pressing one of three buttons.
+        </Typography>
       </Grid>
-      <Grid container item alignItems="center" direction="row">
-        <Grid item xs>
-          <Typography variant="body">
-            Team-project where the end goal was to create a LEGO robot of sorts.
-            We decided to make a street hustler.
-          </Typography>
-        </Grid>
-        <Grid item xs>
-          <img src="./resources/Monte.GIF" alt="Monte video" />
-        </Grid>
+      <Grid container item xs={12} md={6}>
+        <img
+          src="./resources/Monte.GIF"
+          alt="Monte video"
+          style={{ objectFit: "contain" }}
+        />
+        <SkillChips
+          projectName="TDAT1001-LejosProjects/tree/master/KoppOgBall"
+          chips={["Java"]}
+        />
       </Grid>
     </Grid>
   );
