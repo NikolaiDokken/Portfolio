511b5eb47d7adf6583186056bbfbb9df3a7ae743
Nikolai Dokken <nikolaidokken@live.no>
2021-12-08 18:03:41 +0100

    changed label

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -5,7 +5,7 @@ export default function Admin() {
         <div>
             <h2>Admin Page</h2>
             <Link to="/admin/new-project">Add Project</Link>
-            <Link to="/admin/new-job">Add Job</Link>
+            <Link to="/admin/new-job">Add Experience</Link>
         </div>
     );
 }
