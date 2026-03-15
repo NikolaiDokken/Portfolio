ad00ed42b88ab33b8412de855472c0b915745725
NikolaiDokken <nikolaidokken@live.no>
2026-03-15 13:59:13 +0100

    Added post graph and removed posts overview

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  redirects: {
+    "/posts": "/tags",
+  },
   fonts: [
     {
       provider: fontProviders.local(),
